54ea1a8489667505a66b4f70b58b06796b776033
Pearlsea Jain <63936863+pearlsea020@users.noreply.github.com>
2023-02-02 23:12:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@
         source: '/beach-resort-application/:slug*',
         destination: 'https://beach-resort-reservation-react.netlify.app/:slug*',
       },
+     {
+        source: '/online-chat-room',
+        destination: 'https://beach-resort-reservation-react.netlify.app/',
+      },
+      {
+        source: '/online-chat-room/:slug*',
+        destination: 'https://github.com/pearlsea020/Chat-Application:slug*',
+      },
     ]
   },
 };
