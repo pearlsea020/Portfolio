6d7aaee63e7e533a7dea1e73fc97feae69a83d52
Pearlsea Jain <63936863+pearlsea020@users.noreply.github.com>
2023-02-02 23:11:30 -0500

    Updated JSON files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+### Hey there, you found me. 👋
 
+## Find me on my personal site
+- Personal site: https://boonsuen.com
--- next.config.js
+++ next.config.js
@@ -8,36 +8,12 @@
   async rewrites() {
     return [
       {
-        source: '/process-scheduling-solver',
-        destination: 'https://process-scheduling-solver.vercel.app/process-scheduling-solver',
+        source: '/beach-resort-application',
+        destination: 'https://beach-resort-reservation-react.netlify.app/',
       },
       {
-        source: '/process-scheduling-solver/:slug*',
-        destination: 'https://process-scheduling-solver.vercel.app/process-scheduling-solver/:slug*',
-      },
-      {
-        source: '/hodler',
-        destination: 'https://hodler.netlify.app',
-      },
-      {
-        source: '/hodler/:slug*',
-        destination: 'https://hodler.netlify.app/:slug*',
-      },
-      {
-        source: '/tictactoe',
-        destination: 'https://tictactoe-bs.netlify.app',
-      },
-      {
-        source: '/tictactoe/:slug*',
-        destination: 'https://tictactoe-bs.netlify.app/:slug*',
-      },
-      {
-        source: '/hodler-serverful',
-        destination: 'https://hodler-app.herokuapp.com',
-      },
-      {
-        source: '/hodler-serverful/:slug*',
-        destination: 'https://hodler-app.herokuapp.com/:slug*',
+        source: '/beach-resort-application/:slug*',
+        destination: 'https://beach-resort-reservation-react.netlify.app/:slug*',
       },
     ]
   },
